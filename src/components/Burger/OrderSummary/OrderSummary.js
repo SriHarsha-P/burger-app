8b83ba721ccbdcaa91a20a0d1ef759b0e9f29763
PSHarsha <psharsha95@gmail.com>
2020-06-14 02:23:20 -0700

    Burger app complete layout and actions

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,31 +1,34 @@
-import React from 'react';
-import Aux from '../../../hoc/Auxiliary';
+import React,{ Component } from 'react';
+import Aux from '../../../hoc/Auxilary/Auxiliary';
 import Button from '../../UI/Button/Button';
 
+class OrderSummary extends Component {
 
-const orderSummary = (props) => {
+    // This could be functional component, doesn`t necessary to be class
 
-    const ingredientSummary = Object.keys(props.ingredients)
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}>
-                    <span style={{ textTransform: 'capitalize' }}>{igKey} : {props.ingredients[igKey]}</span>
+                    <span style={{ textTransform: 'capitalize' }}>{igKey} : {this.props.ingredients[igKey]}</span>
                 </li>);
         });
 
-    return (
-        <Aux>
+        return(
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients:</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: ${props.price.toFixed(2)}</strong></p>
+            <p><strong>Total Price: ${this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancelled}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
         </Aux>
-    );
+        );
+    }
 };
 
-export default orderSummary;
+export default OrderSummary;
